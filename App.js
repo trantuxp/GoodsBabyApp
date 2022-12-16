9b2e0dedfc456626c2596df552c62f43e4321d30
trantuxp <tqtu.19it2@vku.udn.vn>
2022-12-16 21:15:45 +0700

    tuv10

--- App.js
+++ App.js
@@ -26,11 +26,29 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import MainNavigator from './navigation/ShopNav';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Signin, Register, Homescreen, DetailProduct, Cart} from './screens';
+import DrawerContent from './screens/DrawerContent';
+import Screentest from './screens/Screentest';
 
+const Drawer = createDrawerNavigator();
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
 
 function App() {
+  // return (
+  //   <NavigationContainer>
+  //     <Drawer.Navigator
+  //       // initialRouterName="Homescreen"
+  //       // screenOptions={{headerShown: false}}
+  //       >
+  //       <Drawer.Screen name="Screentest" component={Screentest} />
+  //       <Drawer.Screen name="Cart" component={Cart} />
+  //     </Drawer.Navigator>
+  //   </NavigationContainer>
+  // );
   return <MainNavigator></MainNavigator>;
 }
 
--- index.js
+++ index.js
@@ -5,13 +5,21 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import starRacing from './compoments/starRacing';
 import {
-  login,
+  Signin,
   register,
   ProductItem,
   ProductList,
   Product2Item,
+  Cart,
+  taskbar,
+  Payment,
+  About,
 } from './screens';
+import Comment from './compoments/Comment';
+import MainNavigator from './navigation/ShopNav';
 import Getapi_Flatlist from './Callapi/Getapi_Flatlist';
+import AdminNav from './admin/AdminNav';
 
-AppRegistry.registerComponent(appName, () => Getapi_Flatlist);
+AppRegistry.registerComponent(appName, () => App);
